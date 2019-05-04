3dd88dcbd2122e976b8c1dc9402e0492fe304efa
Mert <mertturkmenoglu99@gmail.com>
2019-05-04 17:32:37 +0300

    Update category header

--- sandproject/app/src/main/java/ce/yildiz/sand/CategoryActivity.java
+++ sandproject/app/src/main/java/ce/yildiz/sand/CategoryActivity.java
@@ -3,6 +3,7 @@ package ce.yildiz.sand;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class CategoryActivity extends AppCompatActivity {
     @Override
@@ -10,7 +11,29 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         Intent intent = getIntent();
+
+        TextView header = findViewById(R.id.categoryActivityHeader);
         int categoryNumber = intent.getIntExtra("category", -1);
+
+        switch (categoryNumber) {
+            case 0:
+                header.setText(getResources().getString(R.string.music));
+                break;
+            case 1:
+                header.setText(getResources().getString(R.string.social));
+                break;
+            case 2:
+                header.setText(getResources().getString(R.string.gaming));
+                break;
+            case 3:
+                header.setText(getResources().getString(R.string.news));
+                break;
+            case 4:
+                header.setText(getResources().getString(R.string.tools));
+                break;
+            case -1:
+                header.setText(getResources().getString(R.string.app_name));
+        }
 
         // TODO: Implement
     }
