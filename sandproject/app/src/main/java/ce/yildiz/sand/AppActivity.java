1e83145f61f677f241f21f204e62d73878f64c5c
Mert <mertturkmenoglu99@gmail.com>
2019-05-04 18:48:01 +0300

    Add AppActivity

---
+++ sandproject/app/src/main/java/ce/yildiz/sand/AppActivity.java
@@ -0,0 +1,12 @@
+package ce.yildiz.sand;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AppActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
